927265217957740f70f1bc1f821e686b31f412c8
arianna choza <unachoza@gmail.com>
2020-10-29 18:15:44 -0700

    front page react app with typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <a className="App-link" rel="noopener noreferrer">
+            Front Page
+          </a>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default App;
