30416448ac8b01634109ef59742d854c801d02f3
arianna choza <unachoza@gmail.com>
2020-11-04 21:56:54 -0800

    working on local storage

--- src/Components/AlgoSingle.tsx
+++ src/Components/AlgoSingle.tsx
@@ -14,18 +14,6 @@ const AlgoSingle: React.FC = () => {
         </p>
         https://repl.it/@unachoza/HuskyVigilantFunction#main.js
         https://repl.it/@unachoza/HandyBurdensomeObservatory#index.js
-        {/* * Definition for a binary tree node.
-         * function TreeNode(val, left, right) {
-         *     this.val = (val===undefined ? 0 : val)
-         *     this.left = (left===undefined ? null : left)
-         *     this.right = (right===undefined ? null : right)
-         * }
-         */
-        /**
-         * @param {TreeNode} t1
-         * @param {TreeNode} t2
-         * @return {TreeNode}
-         */}
         <p className="psudeocode">
           const mergeTrees = function(t1, t2)
           <ul style={{ textAlign: 'left' }}>
--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -3,11 +3,10 @@ import '../App.css';
 
 interface State {
   question: boolean;
-  categories: boolean;
+  datastructure: boolean;
   bigO: boolean;
   psuedoCode: boolean;
   name: false;
-
   array: boolean;
   hashTable: boolean;
   linkedList: boolean;
@@ -19,11 +18,14 @@ interface State {
 
 class Form extends Component<{}, State> {
   state: any = {
-    question: false,
+    question: true,
     categories: false,
     bigO: false,
     psuedoCode: false,
-    passconf: false,
+    algorithm: false,
+    datastructure: true,
+    code: false,
+    pattern: false,
     array: false,
     hashTable: false,
     linkedList: false,
@@ -33,128 +35,244 @@ class Form extends Component<{}, State> {
     queue: false,
   };
   navigate = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    e.preventDefault();
     let navArr = ['question', 'categories', 'bigO', 'psuedoCode'];
     let name: string = e.currentTarget.value;
+    console.log(name, 'this is the target value');
+    console.log(this.state, 'this is state');
     //@ts-ignore
     this.setState({ [name]: true });
     navArr = navArr.filter((nav) => nav !== name);
     //@ts-ignore
     navArr.forEach((nav) => this.setState({ [nav]: false }));
   };
+
   toggleFork = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
     let datastructure = e.currentTarget.value;
+    console.log(datastructure, 'this is the target value');
     //@ts-ignore
     this.setState({ [datastructure]: !this.state.datastructure });
   };
 
   render() {
+    let questions: any[] = ['start'];
+
+    // // v: React.ChangeEvent<HTMLInputElement>,
+    const saveToLocalStorage = (e: any) => {
+      questions.push(e);
+      console.log(questions);
+      localStorage.setItem('questions', JSON.stringify(questions));
+    };
+
+    console.log(this.state);
     const dataStructures: string[] = ['array', 'hashTable', 'linkedList', 'tree', 'graph', 'stack', 'queue'];
     const AlgorithmTypes: string[] = ['sorting', 'searching', 'counting', 'bubble'];
+    const patterns: string[] = ['divide & conquer', 'sliding window', 'frequencey counter', 'multiple pointers'];
+    const { question, datastructure, pattern, psuedoCode, bigO, algorithm, toggleFork, code } = this.state;
     return (
-      <div className="container">
-        <form action="/register" method="POST" className="form">
-          {/* page1 */}
-          <div>
-            Add Question
-            <div className="form-group" id="name">
-              <textarea rows={10} name="Username" id="username" className="form-control" placeholder="question" />
-            </div>
-            <button value="categories" type="submit" className="btn-submit " onClick={(e) => this.navigate(e)}>
-              Next
-            </button>
-          </div>
-          {/* page 2 */}
-          <div>
-            Select Data Structure
-            <div className="data-btn-container">
-              <div className="data-group" id="phone-inp">
-                {dataStructures.map((type, i) => (
-                  <div key={i} className="btn">
-                    <input
-                      type="checkbox"
-                      id={type}
-                      name={type}
-                      value={type}
-                      hidden
-                      onClick={(e) => this.state.toggleFork(e)}
+      <>
+        <div className="container">
+          <form action="/register" method="POST" className="form">
+            {/* page1 */}
+            {question ? (
+              <>
+                <div>
+                  Add Question
+                  <div className="form-group" id="name">
+                    <textarea
+                      rows={10}
+                      name="Username"
+                      id="quest"
+                      className="form-control"
+                      placeholder="question"
+                      onBlur={(ev: React.ChangeEvent<HTMLTextAreaElement>) => saveToLocalStorage(ev.target.value)}
                     />
-                    <label htmlFor={type} id="checkbox-option">
-                      {type}
-                    </label>
+                    <div>
+                      <button
+                        value="categories"
+                        type="submit"
+                        className="btn-submit "
+                        onClick={(e) => this.navigate(e)}
+                      >
+                        Next
+                      </button>
+                    </div>
                   </div>
-                ))}
-              </div>
-            </div>
-            Identify Algorithm
-            <div className="data-btn-container">
-              <div className="data-group" id="phone-inp">
-                {AlgorithmTypes.map((type, i) => (
-                  <div key={i} className="btn">
-                    <input
-                      type="checkbox"
-                      id={type}
-                      name={type}
-                      value={type}
-                      hidden
-                      onClick={(e) => this.state.toggleFork(e)}
-                    />
-                    <label htmlFor={type} id="checkbox-option">
-                      {type}
-                    </label>
-                  </div>
-                ))}
-              </div>
-            </div>
-            <button value="categories" type="submit" className="btn-submit " onClick={(e) => this.navigate(e)}>
-              Back
-            </button>
-            <button value="categories" type="submit" className="btn-submit " onClick={(e) => this.navigate(e)}>
-              Next
-            </button>
-          </div>
-          {/* page3 */}
-          <div>
-            Pseudocode
-            <div className="form-group" id="name">
-              <textarea rows={10} name="Username" id="username" className="form-control" placeholder="question" />
-            </div>
-            <button value="categories" type="submit" className="btn-submit " onClick={(e) => this.navigate(e)}>
-              Back
-            </button>
-            <button value="categories" type="submit" className="btn-submit " onClick={(e) => this.navigate(e)}>
-              Next
-            </button>
-          </div>
-          {/* page4 */}
-          <div>
-            Code
-            <div className="form-group" id="name">
-              <textarea rows={10} name="Username" id="username" className="form-control" placeholder="question" />
-            </div>
-            <button value="categories" type="submit" className="btn-submit " onClick={(e) => this.navigate(e)}>
-              Back
-            </button>
-            <button value="categories" type="submit" className="btn-submit " onClick={(e) => this.navigate(e)}>
-              Next
-            </button>
-          </div>
-          {/* page5 */}
-          <div>
-            Big O
-            <div className="form-group" id="name">
-              <input name="Username" id="username" className="form-control" placeholder="Time" />
-              <input name="Username" id="username" className="form-control" placeholder="Space" />
-            </div>
-            <button value="categories" type="submit" className="btn-submit " onClick={(e) => this.navigate(e)}>
-              Back
-            </button>
-            <button value="categories" type="submit" className="btn-submit " onClick={(e) => this.navigate(e)}>
-              Submit
-            </button>
-          </div>
-        </form>
-        <form action="submit"></form>
-      </div>
+                  {datastructure ? (
+                    <div>
+                      Select Data Structure
+                      <div className="data-btn-container">
+                        <div className="data-group" id="phone-inp">
+                          {dataStructures.map((type, i) => (
+                            <div key={i} className="btn">
+                              <input
+                                type="checkbox"
+                                id={type}
+                                name={type}
+                                value={type}
+                                hidden
+                                onClick={(e) => toggleFork(e)}
+                              />
+                              <label htmlFor={type} id="checkbox-option">
+                                {type}
+                              </label>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  ) : null}
+                  {algorithm ? (
+                    <div>
+                      Identify Algorithm
+                      <div className="data-btn-container">
+                        <div className="data-group" id="phone-inp">
+                          {AlgorithmTypes.map((type, i) => (
+                            <div key={i} className="btn">
+                              <input
+                                type="checkbox"
+                                id={type}
+                                name={type}
+                                value={type}
+                                hidden
+                                onClick={(e) => toggleFork(e)}
+                              />
+                              <label htmlFor={type} id="checkbox-option">
+                                {type}
+                              </label>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  ) : null}
+                  {pattern ? (
+                    <div>
+                      Identify Problem Solving Pattern
+                      <div className="data-btn-container">
+                        <div className="data-group" id="phone-inp">
+                          {patterns.map((type, i) => (
+                            <div key={i} className="btn">
+                              <input
+                                type="checkbox"
+                                id={type}
+                                name={type}
+                                value={type}
+                                hidden
+                                onClick={(e) => toggleFork(e)}
+                              />
+                              <label htmlFor={type} id="checkbox-option">
+                                {type}
+                              </label>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                      <button value="question" type="submit" className="btn-submit " onClick={(e) => this.navigate(e)}>
+                        Back
+                      </button>
+                      <button
+                        value="psuedoCode"
+                        type="submit"
+                        className="btn-submit "
+                        onClick={(e) => this.navigate(e)}
+                      >
+                        Next
+                      </button>
+                    </div>
+                  ) : null}
+                  {psuedoCode ? (
+                    <div>
+                      Pseudocode
+                      <div className="form-group" id="name">
+                        <textarea
+                          rows={10}
+                          name="Username"
+                          id="username"
+                          className="form-control"
+                          placeholder="question"
+                        />
+                      </div>
+                      <button
+                        value="categories"
+                        type="submit"
+                        className="btn-submit "
+                        onClick={(e) => this.navigate(e)}
+                      >
+                        Back
+                      </button>
+                      <button
+                        value="categories"
+                        type="submit"
+                        className="btn-submit "
+                        onClick={(e) => this.navigate(e)}
+                      >
+                        Next
+                      </button>
+                    </div>
+                  ) : null}
+                  {code ? (
+                    <div>
+                      Code
+                      <div className="form-group" id="name">
+                        <textarea
+                          rows={10}
+                          name="Username"
+                          id="username"
+                          className="form-control"
+                          placeholder="question"
+                        />
+                      </div>
+                      <button
+                        value="categories"
+                        type="submit"
+                        className="btn-submit "
+                        onClick={(e) => this.navigate(e)}
+                      >
+                        Back
+                      </button>
+                      <button
+                        value="categories"
+                        type="submit"
+                        className="btn-submit "
+                        onClick={(e) => this.navigate(e)}
+                      >
+                        Next
+                      </button>
+                    </div>
+                  ) : null}
+                  {bigO ? (
+                    <div>
+                      Big O
+                      <div className="form-group" id="name">
+                        <input name="Username" id="username" className="form-control" placeholder="Time" />
+                        <input name="Username" id="username" className="form-control" placeholder="Space" />
+                      </div>
+                      <button
+                        value="categories"
+                        type="submit"
+                        className="btn-submit "
+                        onClick={(e) => this.navigate(e)}
+                      >
+                        Back
+                      </button>
+                      <button
+                        value="categories"
+                        type="submit"
+                        className="btn-submit "
+                        onClick={(e) => this.navigate(e)}
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  ) : null}
+                </div>
+              </>
+            ) : null}
+          </form>
+          <form action="submit"></form>
+        </div>
+      </>
     );
   }
 }
